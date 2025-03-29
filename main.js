a985d8c2ae8a6cd4d3e4d59cac19f163d5b5a530
Daxz <59543649+Daxz0@users.noreply.github.com>
2025-03-29 11:17:34 -0700

    + test test

--- main.js
+++ main.js
@@ -7,7 +7,13 @@ kaplay({
 })
 
 
+// define game variables
 
+
+let waterLevel = 0
+
+
+loadSprite("start_bg", "sprites/start_bg.png")
 scene("start", () => {
     add([
         text("60 Seconds Till Drought", { size: 48 }),
@@ -46,11 +52,49 @@ scene("start", () => {
     ]);
 
     onClick("start", () => {
-        go("game");
+        go("house");
     });
+    add([
+        sprite("start_bg"),
+        pos(0, 0),
+        scale(width() / sprite("start_bg"),").width, height() / sprite("start_bg"),").height),
+        fixed(),
+    ]);
 
     // onClick("quit", () => {
     // });
+});
+
+
+scene("house", () => {
+    add([
+        rect(220, 32),
+        pos(20, 20),
+        color(0, 0, 0),
+        "waterBarBg",
+    ]);
+
+    const waterBar = add([
+        rect(220, 32),
+        pos(20, 20),
+        color(0, 0, 255),
+        "waterBar",
+    ]);
+
+    function updateWaterBar() {
+        waterBar.width = (waterLevel / 100) * 220; // Scale width based on water level
+    }
+
+    // Example of decreasing water level over time
+    loop(1, () => {
+        if (waterLevel > 0) {
+            waterLevel -= 1; // Decrease water level
+            updateWaterBar(); // Update the water bar
+        }
+    });
+
+    // Call the update function to set the initial state
+    updateWaterBar();
 });
 
 
