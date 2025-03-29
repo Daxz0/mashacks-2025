3aa233a8bd8c510700172df30d8c4ca1eb1e90a6
Daxz <59543649+Daxz0@users.noreply.github.com>
2025-03-29 14:05:46 -0700

    + UPDATEEE

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 import kaplay from "./kaplay.mjs";
 
 kaplay({
-    font: "sans-serif",
-    canvas: document.querySelector("#mycanvas"),
-    background: [0, 0, 255],
+    background: [0, 0, 0],
 });
 
 // Define game variables
 let waterLevel = 100;
+let health = 100;
+let happiness = 100;
+let hygiene = 100;
 let day = 1;
-
+loadSprite("house_bg", "sprites/house_bg.jpg")
 loadSprite("start_bg", "sprites/start_bg.jpg").then(() => {
     scene("start", () => {
         const background = add([
@@ -18,11 +19,10 @@ loadSprite("start_bg", "sprites/start_bg.jpg").then(() => {
             fixed(),
         ]);
 
-        // Scale the background to fit the canvas
         background.scale = vec2(width() / background.width, height() / background.height);
 
         add([
-            text("60 Seconds Till Drought", { size: 100 }),
+            text("The Watermaster Strikes!", { size: 100 }),
             pos(center().x, 250),
             anchor("center"),
         ]);
@@ -46,48 +46,30 @@ loadSprite("start_bg", "sprites/start_bg.jpg").then(() => {
         onClick("start", () => {
             go("house");
         });
-
-
     });
 
     go("start");
 });
 
+function heatwaveEffect() {
+    waterLevel -= 80;
+}
 
-scene("house", () => {
-    add([
-        text("House", { size: 100 }),
-        pos(center().x, center().y),
-        anchor("center"),
-    ]);
+function findWellEffect() {
+    waterLevel += 3;
+}
 
-    const waterMeter = add([
-        rect(100,600),
-        anchor("left"),
-        "water",
-    ])
+function normalDayEffect() {}
 
-
-    const nextDayButton = add([
-        rect(400, 100),
-        pos(center().x, 500),
-        anchor("center"),
-        area(),
-        "nextDay",
-    ]);
-
-    onClick("nextDay", () => {
-        
-        day += 1;
-    });
-
-});
+function banditAttackEffect() {
+    waterLevel -= 1;
+}
 
 const events = [
-    { text: "A heatwave hits! Lose 2 water.", effect: () => waterLevel -= 2 },
-    { text: "You find an old well! Gain 3 water.", effect: () => waterLevel += 3 },
-    { text: "You have a normal day. No changes.", effect: () => {} },
-    { text: "Bandits steal some water! Lose 1 water.", effect: () => waterLevel -= 1 },
+    { text: "A heatwave hits! Lose 2 water.", effect: heatwaveEffect },
+    { text: "You find an old well! Gain 3 water.", effect: findWellEffect },
+    { text: "You have a normal day. No changes.", effect: normalDayEffect },
+    { text: "Bandits steal some water! Lose 1 water.", effect: banditAttackEffect },
 ];
 
 function randomEvent() {
@@ -97,22 +79,90 @@ function randomEvent() {
 }
 
 scene("house", () => {
+
+    const background = add([
+        sprite("house_bg"),
+        pos(0, 0),
+        fixed(),
+    ]);
+
+    background.scale = vec2(width() / background.width, height() / background.height);
+
     const dayText = add([
         text(`Day ${day}`, { size: 50 }),
         pos(50, 50),
     ]);
 
-    const waterDisplay = add([
-        text(`Water: ${waterLevel}`, { size: 50 }),
-        pos(50, 100),
-    ]);
-
     const eventText = add([
-        text("What's going to happen today?", { size: 30 }),
+        text("The water has been shut down...", { size: 30 }),
         pos(50, 200),
     ]);
+    add([
+        rect(90, 300),
+        pos(10, height() - 310),
+        anchor("left"),
+        "water",
+        color(0, 0, 0),
+    ]);
 
-    const nextDayButton = add([
+    const waterMeter = add([
+        rect(90, 300),
+        pos(10, height() - 310),
+        anchor("left"),
+        "water",
+        color(0, 0, 255),
+    ]);
+
+    const healthBar = add([
+        rect(200, 20),
+        pos(width()-25, 70),
+        anchor("topright"),
+        "health",
+        color(0, 255, 0),
+    ]);
+
+    const hygieneBar = add([
+        rect(200, 20),
+        pos(width()-25, 120),
+        anchor("topright"),
+        "hygiene",
+        color(255, 255, 0),
+    ]);
+
+    const happinessBar = add([
+        rect(200, 20),
+        pos(width()-25, 170),
+        anchor("topright"),
+        "happiness",
+        color(255, 165, 0),
+    ]);
+
+    const waterDisplay = add([
+        text(`Water: ${waterLevel}`, { size: 50 }),
+        pos(45, 280),
+        anchor("left"),
+        rotate(),
+    ]);
+
+    add([
+        text("Health", { size: 20 }),
+        pos(width()-25, 50),
+        anchor("topright"),
+    ]);
+
+    add([
+        text("Hygiene", { size: 20 }),
+        pos(width()-25, 100),
+        anchor("topright"),
+    ]);
+
+    add([
+        text("Happiness", { size: 20 }),
+        pos(width()-25, 150),
+        anchor("topright"),
+    ]);
+
+    add([
         rect(400, 100),
         pos(center().x, 500),
         anchor("center"),
@@ -128,15 +178,27 @@ scene("house", () => {
     ]);
 
     onClick("nextDay", () => {
+        if (day >= 30) {
+            go("gameWin");
+            return;
+        }
         day++;
         let eventMessage = randomEvent();
         eventText.text = eventMessage;
+        dayText.text = "Day " + day;
         waterDisplay.text = `Water: ${waterLevel}`;
+
+        waterMeter.height = 300 * (waterLevel / 100);
+        healthBar.width = 200 * (health / 100);
+        hygieneBar.width = 200 * (hygiene / 100);
+        happinessBar.width = 200 * (happiness / 100);
 
         if (waterLevel <= 0) {
             go("gameOver");
         }
     });
+
+    waterDisplay.angle = 90;
 });
 
 scene("gameOver", () => {
@@ -153,7 +215,16 @@ scene("gameOver", () => {
     ]);
 });
 
-go("start");
+scene("gameWin", () => {
+    add([
+        text("You survived the Watermaster's wrath!", { size: 65 }),
+        pos(center().x, center().y),
+        anchor("center"),
+    ]);
 
-
-onUpdate("dayText")
+    add([
+        text("Congratulations!", { size: 50 }),
+        pos(center().x, center().y + 100),
+        anchor("center"),
+    ]);
+});
