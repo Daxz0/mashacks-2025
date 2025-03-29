be612f023318e4a3fbf6266327c357beb1048a29
Daxz <59543649+Daxz0@users.noreply.github.com>
2025-03-29 11:06:20 -0700

    + update

--- main.js
+++ main.js
@@ -1,19 +1,57 @@
 import kaplay from "./kaplay.mjs";
 
-kaplay()
-
-
-const obj = add([
-    // this is a component that draws a rectangle
-    rect(32, 32),
-]);
-
-
-onUpdate(() => {
-    console.log("update")
+kaplay({
+    font: "sans-serif",
+    canvas: document.querySelector("#mycanvas"),
+    background: [ 0, 0, 255, ],
 })
 
 
-onKeyPress("e", () => {
-    console.log("e pressed")
+
+scene("start", () => {
+    add([
+        text("60 Seconds Till Drought", { size: 48 }),
+        pos(center().x, 100),
+        anchor("center"),
+    ]);
+
+    const startButton = add([
+        rect(200, 50),
+        pos(center().x, 200),
+        anchor("center"),
+        area(),
+        "start",
+    ]);
+
+    add([
+        text("Start", { size: 24 }),
+        pos(center().x, 200),
+        anchor("center"),
+        color(0, 0, 0),
+    ]);
+
+    const quitButton = add([
+        rect(200, 50),
+        pos(center().x, 300),
+        anchor("center"),
+        area(),
+        "quit",
+    ]);
+
+    add([
+        text("Quit", { size: 24 }),
+        pos(center().x, 300),
+        anchor("center"),
+        color(0, 0, 0),
+    ]);
+
+    onClick("start", () => {
+        go("game");
+    });
+
+    // onClick("quit", () => {
+    // });
 });
+
+
+go("start");
